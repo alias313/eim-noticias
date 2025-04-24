79356815db9dea55c5ab1b1db007d95ca1a615ce
Elies <eliesgarciaalvira@gmail.com>
2025-04-24 23:32:55 +0200

    Graceful fail when null image

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,15 +1,13 @@
 import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import type { ImageProps } from 'next/image';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    img: (props) => {
-      if (!props.src) return null;
-      <ImageZoom {...(props as any)} />;
-    },
+    img: (props?: Partial<ImageProps>) => <ImageZoom {...(props as any)} />,
     ...components,
   };
 }
