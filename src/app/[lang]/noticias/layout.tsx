baa83a2cf77f95f1c8d0874e3e38f6963a3ab063
Elies <eliesgarciaalvira@gmail.com>
2025-04-25 02:54:50 +0200

    Added docsOptions

--- src/app/[lang]/noticias/layout.tsx
+++ src/app/[lang]/noticias/layout.tsx
@@ -1,21 +1,29 @@
 "use client";
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import type { ReactNode } from 'react';
+import { use, type ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
 import { useMousedownNavigation } from '@/components/useMousedownNavigation';
 import { useParams } from 'next/navigation';
+import { DocsLayoutProps } from 'fumadocs-ui/layouts/notebook';
+import { GithubInfo } from 'fumadocs-ui/components/github-info';
 
 export default function Layout({ children }: { children: ReactNode }) {
   const layoutRef = useMousedownNavigation();
   const params = useParams();
   const lang = params.lang as string;
 
+  const docsOptions: DocsLayoutProps = {
+    ...baseOptions,
+    tree: source.pageTree[lang],
+    links: [
+    ],
+  };
+  
   return (
     <div ref={layoutRef}>
       <DocsLayout
-        tree={source.pageTree[lang]}
-        {...baseOptions}
+        {...docsOptions}
       >
         {children}
       </DocsLayout>
